4b09f6a8fa3718f4a9e3195871cc94c275c2080b
Milan Staněk <mistacms@gmail.com>
2016-10-13 22:29:12 +0200

    Milan Stanek - strankovani seznamu po 10

--- src/AppBundle/Controller/HomepageController.php
+++ src/AppBundle/Controller/HomepageController.php
@@ -1,5 +1,6 @@
 <?php
 namespace AppBundle\Controller;
+
 use AppBundle\Entity\Category;
 use AppBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,42 +12,53 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class HomepageController extends Controller
 {
-
-	/**
-	 * @Route("/", name="homepage")
-	 * @param Request $request
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function homepageAction(Request $request)
-	{
-		return $this->render("homepage/homepage.html.twig", [
-			"products" => $this->getDoctrine()->getRepository(Product::class)->findBy(
-				[],
-				[
-					"rank" => "desc"
-				],
-				20
-			),
-			"categories" => $this->getDoctrine()->getRepository(Category::class)->findBy(
-				[],
-				[
-					"rank" => "desc",
-				]
-			),
-		]);
-	}
-
-
-	/**
-	 * @Route("/hello", name="hello")
-	 * @param Request $request
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function helloAction(Request $request)
-	{
-		return $this->render("homepage/hello.html.twig", [
-			"who" => "world",
-		]);
-	}
-
+    
+    /**
+     * @Route("/", name="homepage")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function homepageAction(Request $request)
+    {
+    
+        $paginator  = $this->get('knp_paginator');
+        $em = $this->getDoctrine()->getManager();
+        $all = $em->getRepository(Product::class)->findAll();
+        $products = $paginator->paginate(
+            $all,
+            $request->query->getInt('page', 1),
+            9
+        );
+    
+        return $this->render(
+            "homepage/homepage.html.twig",
+            [
+                "products" => $products
+                ,
+                "categories" => $em->getRepository(Category::class)->findBy(
+                    [],
+                    [
+                        "rank" => "desc",
+                    ]
+                ),
+            ]
+        );
+    }
+    
+    
+    /**
+     * @Route("/hello", name="hello")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function helloAction(Request $request)
+    {
+        return $this->render(
+            "homepage/hello.html.twig",
+            [
+                "who" => "world",
+            ]
+        );
+    }
+    
 }
