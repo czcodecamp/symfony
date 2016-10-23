97992aed8df9b123416985ff7797206609004201
Aleku22 <a.kudela@seznam.cz>
2016-10-24 00:29:31 +0200

    - základní úkol

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace AppBundle\Controller;
+
 use AppBundle\Entity\User;
 use AppBundle\Facade\UserFacade;
 use AppBundle\FormType\RegistrationFormType;
+use AppBundle\FormType\UserEditFormType;
 use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -13,104 +16,118 @@ use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
-
 /**
  * @author Vašek Boch <vasek.boch@live.com>
  * @author Jan Klat <jenik@klatys.cz>
  * @Route(service="app.controller.user_controller")
  */
-class UserController
-{
-	private $userFacade;
-	private $formFactory;
-	private $passwordEncoder;
-	private $entityManager;
-	private $router;
+class UserController {
 
-	public function __construct(
-		UserFacade $userFacade,
-		FormFactory $formFactory,
-		PasswordEncoderInterface $passwordEncoder,
-		EntityManager $entityManager,
-		RouterInterface $router
-	) {
-		$this->userFacade = $userFacade;
-		$this->formFactory = $formFactory;
-		$this->passwordEncoder = $passwordEncoder;
-		$this->entityManager = $entityManager;
-		$this->router = $router;
-	}
+    private $userFacade;
+    private $formFactory;
+    private $passwordEncoder;
+    private $entityManager;
+    private $router;
 
-	/**
-	 * @Route("/registrovat", name="user_registration")
-	 * @Template("user/registration.html.twig")
-	 *
-	 * @param Request $request
-	 * @return RedirectResponse|array
-	 */
-	public function registrationAction(Request $request)
-	{
-		// 1) build the form
-		$user = new User();
-		$form = $this->formFactory->create(RegistrationFormType::class, $user);
+    public function __construct(
+    UserFacade $userFacade, FormFactory $formFactory, PasswordEncoderInterface $passwordEncoder, EntityManager $entityManager, RouterInterface $router
+    ) {
+        $this->userFacade = $userFacade;
+        $this->formFactory = $formFactory;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->entityManager = $entityManager;
+        $this->router = $router;
+    }
 
-		// 2) handle the submit (will only happen on POST)
-		$form->handleRequest($request);
-		if ($form->isSubmitted() && $form->isValid()) {
+    /**
+     * @Route("/registrovat", name="user_registration")
+     * @Template("user/registration.html.twig")
+     *
+     * @param Request $request
+     * @return RedirectResponse|array
+     */
+    public function registrationAction(Request $request) {
+        // 1) build the form
+        $user = new User();
+        $form = $this->formFactory->create(RegistrationFormType::class, $user);
 
-			// 3) Encode the password (you could also do this via Doctrine listener)
-			$user->setPassword(
-				$this->passwordEncoder->encodePassword($user->getPlainPassword(), null)
-			);
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
 
-			// 4) save the User!
-			$this->entityManager->persist($user);
-			$this->entityManager->flush();
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $user->setPassword(
+                    $this->passwordEncoder->encodePassword($user->getPlainPassword(), null)
+            );
 
-			// ... do any other work - like sending them an email, etc
-			// maybe set a "flash" success message for the user
-			return RedirectResponse::create($this->router->generate("homepage"));
-		}
+            // 4) save the User!
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
 
-		return [
-			"form" => $form->createView(),
-		];
-	}
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+            return RedirectResponse::create($this->router->generate("homepage"));
+        }
 
-	/**
-	 * @Route("/prihlasit", name="user_login")
-	 * @Template("user/login.html.twig")
-	 *
-	 * @return RedirectResponse|array
-	 */
-	public function loginAction()
-	{
-		return [
-			"last_username" => $this->userFacade->getLastUsername(),
-			"error" => $this->userFacade->getAuthenticationError(),
-		];
-	}
+        return [
+            "form" => $form->createView(),
+        ];
+    }
 
-	/**
-	 * @Route("/odhlasit", name="user_logout")
-	 */
-	public function logoutAction()
-	{}
+    /**
+     * @Route("/prihlasit", name="user_login")
+     * @Template("user/login.html.twig")
+     *
+     * @return RedirectResponse|array
+     */
+    public function loginAction() {
+        return [
+            "last_username" => $this->userFacade->getLastUsername(),
+            "error" => $this->userFacade->getAuthenticationError(),
+        ];
+    }
 
-	/**
-	 * @Route("/uzivatel/super-tajna-stranka", name="supersecret")
-	 * @Template("user/supersecret.html.twig")
-	 *
-	 * @return array
-	 */
-	public function superSecretAction()
-	{
-		if (!$this->userFacade->getUser()) {
-			throw new UnauthorizedHttpException("Přihlašte se");
-		}
-		return [
-			"user" => $this->userFacade->getUser(),
-		];
-	}
+    /**
+     * @Route("/odhlasit", name="user_logout")
+     */
+    public function logoutAction() {
+        
+    }
+
+    /**
+     * @Route("/uzivatel/super-tajna-stranka", name="supersecret")
+     * @Template("user/supersecret.html.twig")
+     * @param Request $request
+     * @return array
+     */
+    public function superSecretAction(Request $request) {
+        if (!$this->userFacade->getUser()) {
+            throw new UnauthorizedHttpException("Přihlašte se");
+        }
+
+        // 1) build the form
+        $user = $this->userFacade->getUser();
+        $form = $this->formFactory->create(UserEditFormType::class, $user);
+
+        $user->setPlainPassword($user->getPassword());
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            // 4) save the User!
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+            return RedirectResponse::create($this->router->generate("homepage"));
+        }
+
+        return [
+            "form" => $form->createView(),
+            "user" => $this->userFacade->getUser(),
+        ];
+    }
 
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,132 +1,280 @@
 <?php
+
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @author Vašek Boch <vasek.boch@live.com>
  * @author Jan Klat <jenik@klatys.cz>
+ * @author Aleš Kůdela
  * @ORM\Entity
  * @UniqueEntity(fields="username", message="Tento e-mail je již registrován")
  */
-class User implements UserInterface
-{
+class User implements UserInterface {
 
-	/**
-	 * @var int
-	 * @ORM\Id
-	 * @ORM\GeneratedValue
-	 * @ORM\Column(type="integer")
-	 */
-	private $id;
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
-	/**
-	 * @ORM\Column(type="string", length=255, unique=true, name="email")
-	 * @Assert\NotBlank()
-	 * @Assert\Email()
-	 */
-	private $username;
+    /**
+     * @ORM\Column(type="string", length=255, unique=true, name="email")
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $username;
 
-	/**
-	 * @ORM\Column(type="string", length=64)
-	 */
-	private $password;
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
 
-	/**
-	 * @Assert\NotBlank()
-	 * @Assert\Length(max=4096)
-	 */
-	private $plainPassword;
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=4096)
+     */
+    private $plainPassword;
 
-	/**
-	 * @return int
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true)
+     */
+    private $phone;
 
-	/**
-	 * @param int $id
-	 * @return self
-	 */
-	public function setId($id)
-	{
-		$this->id = $id;
-		return $this;
-	}
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=64)
+     */
+    private $title;
 
-	/**
-	 * @return mixed
-	 */
-	public function getUsername()
-	{
-		return $this->username;
-	}
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=64)
+     */
+    private $name;
 
-	/**
-	 * @param mixed $username
-	 * @return self
-	 */
-	public function setUsername($username)
-	{
-		$this->username = $username;
-		return $this;
-	}
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
 
-	/**
-	 * @return mixed
-	 */
-	public function getPassword()
-	{
-		return $this->password;
-	}
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
 
-	/**
-	 * @param mixed $password
-	 * @return self
-	 */
-	public function setPassword($password)
-	{
-		$this->password = $password;
-		return $this;
-	}
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=16)
+     */
+    private $zip;
 
-	/**
-	 * @return mixed
-	 */
-	public function getPlainPassword()
-	{
-		return $this->plainPassword;
-	}
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
 
-	/**
-	 * @param mixed $plainPassword
-	 * @return self
-	 */
-	public function setPlainPassword($plainPassword)
-	{
-		$this->plainPassword = $plainPassword;
-		return $this;
-	}
+    public function __construct() {
+        $this->userAddresses = new ArrayCollection;
+    }
 
-	public function getRoles()
-	{
-		return [];
-	}
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
 
-	public function getSalt()
-	{
-		return null;
-	}
+    /**
+     * @param int $id
+     * @return self
+     */
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
 
-	public function eraseCredentials()
-	{
-		//nothing to do
-		return;
-	}
+    /**
+     * @return mixed
+     */
+    public function getUsername() {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     * @return self
+     */
+    public function setUsername($username) {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone() {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     * @return self
+     */
+    public function setPhone($phone) {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     * @return self
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName() {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $name
+     * @return self
+     */
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZip() {
+        return $this->zip;
+    }
+
+    /**
+     * @param mixed $zip
+     * @return self
+     */
+    public function setZip($zip) {
+        $this->zip = $zip;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreet() {
+        return $this->street;
+    }
+
+    /**
+     * @param mixed $street
+     * @return self
+     */
+    public function setStreet($street) {
+        $this->street = $street;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity() {
+        return $this->street;
+    }
+
+    /**
+     * @param mixed $city
+     * @return self
+     */
+    public function setCity($city) {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry() {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     * @return self
+     */
+    public function setCountry($country) {
+        $this->country = $country;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword() {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     * @return self
+     */
+    public function setPassword($password) {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPlainPassword() {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param mixed $plainPassword
+     * @return self
+     */
+    public function setPlainPassword($plainPassword) {
+        $this->plainPassword = $plainPassword;
+        return $this;
+    }
+
+    public function getRoles() {
+        return [];
+    }
+
+    public function getSalt() {
+        return null;
+    }
+
+    public function eraseCredentials() {
+        //nothing to do
+        return;
+    }
 
 }
